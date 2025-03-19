8188d282c931b9a2a42dcbfe44ef5c5ae7170060
Ben Purinton <ben.purinton@gmail.com>
2025-03-18 21:44:09 -0700

    Ajaxify comments#edit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,10 @@ class CommentsController < ApplicationController
 
   # GET /comments/1/edit
   def edit
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   # POST /comments or /comments.json
